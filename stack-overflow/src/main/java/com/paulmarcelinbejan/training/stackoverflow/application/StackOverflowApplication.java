3c641a3b836edfa38000c8c3c1789eb2b8d3b4b5
Paul Marcelin Bejan <paulmarcelinbejan.developer@gmail.com>
2023-02-14 22:29:05 +0100

    feat: maven modules

---
+++ stack-overflow/src/main/java/com/paulmarcelinbejan/training/stackoverflow/application/StackOverflowApplication.java
@@ -0,0 +1,13 @@
+package com.paulmarcelinbejan.training.stackoverflow.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackOverflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StackOverflowApplication.class, args);
+    }
+
+}
