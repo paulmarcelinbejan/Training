e631aa5b178076dbd78d5d7e82de3978b7c56480
Paul Marcelin Bejan <paulmarcelinbejan.developer@gmail.com>
2023-05-22 07:44:33 +0200

    style: removed unused import

--- stack-overflow/src/main/java/com/paulmarcelinbejan/training/stackoverflow/mapstruct/custommapping/Q1/AnimalMapper.java
+++ stack-overflow/src/main/java/com/paulmarcelinbejan/training/stackoverflow/mapstruct/custommapping/Q1/AnimalMapper.java
@@ -2,7 +2,6 @@ package com.paulmarcelinbejan.training.stackoverflow.mapstruct.custommapping.Q1;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
@@ -12,38 +11,41 @@ import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface AnimalMapper extends BaseMapper<Animal, AnimalResponse> {
-	
-    AnimalMapper INSTANCE = Mappers.getMapper(AnimalMapper.class);
-    
-    @Override
-    @Named("toDto")
-    @Mapping(source = "animalTypes", target = "animalTypes", qualifiedByName = "toArray")
-    AnimalResponse toDto(Animal entity);
-    
-    @Override
-    @IterableMapping(qualifiedByName = "toDto")
-    List<AnimalResponse> toDtoList(List<Animal> entityList);
-    
-    @Override
-    @Named("toEntity")
-    @Mapping(source = "animalTypes", target = "animalTypes", qualifiedByName = "toArrayList")
-    Animal toEntity(AnimalResponse dto);
-    
-    @IterableMapping(qualifiedByName = "toEntity")
-    List<Animal> toEntityList(List<AnimalResponse> dtoList);
-    
-    @Named("toArray") 
-    default Long[] toArray(List<AnimalType> animalTypes) { 
-        return animalTypes.stream()
-        				  .map(AnimalType::getId)
-        				  .toArray(size -> new Long[size]);
-    }
-    
-    @Named("toArrayList") 
-    default List<AnimalType> toArrayList(Long[] animalTypes) { 
-        return Arrays.stream(animalTypes)
-   			 		 .map(AnimalType::new)
-   			 		 .collect(Collectors.toList());
-    }
-    
+
+	AnimalMapper INSTANCE = Mappers.getMapper(AnimalMapper.class);
+
+	@Override
+	@Named("toDto")
+	@Mapping(source = "animalTypes", target = "animalTypes", qualifiedByName = "toArray")
+	AnimalResponse toDto(Animal entity);
+
+	@Override
+	@IterableMapping(qualifiedByName = "toDto")
+	List<AnimalResponse> toDtoList(List<Animal> entityList);
+
+	@Override
+	@Named("toEntity")
+	@Mapping(source = "animalTypes", target = "animalTypes", qualifiedByName = "toArrayList")
+	Animal toEntity(AnimalResponse dto);
+
+	@Override
+	@IterableMapping(qualifiedByName = "toEntity")
+	List<Animal> toEntityList(List<AnimalResponse> dtoList);
+
+	@Named("toArray")
+	default Long[] toArray(List<AnimalType> animalTypes) {
+		return animalTypes
+				.stream()
+				.map(AnimalType::getId)
+				.toArray(size -> new Long[size]);
+	}
+
+	@Named("toArrayList")
+	default List<AnimalType> toArrayList(Long[] animalTypes) {
+		return Arrays
+				.stream(animalTypes)
+				.map(AnimalType::new)
+				.toList();
+	}
+
 }
