9609f2d33f8903a56af369bea13b456162ae329b
Paul Marcelin Bejan <paulmarcelinbejan.developer@gmail.com>
2023-12-10 12:47:45 +0100

    feat: TestC and TestD

---
+++ interview/src/test/java/interview/TestC.java
@@ -0,0 +1,28 @@
+package interview;
+
+import org.junit.jupiter.api.Test;
+
+class TestC {
+
+	@Test
+	void test1() {
+		
+		System.out.println(10 + 10 + "€");
+		
+	}
+	
+	@Test
+	void test2() {
+		
+		System.out.println("€" + 10 + 10);
+		
+	}
+	
+	@Test
+	void test3() {
+		
+		System.out.println("€" + 10 * 10);
+		
+	}
+	
+}
---
+++ interview/src/test/java/interview/TestD.java
@@ -0,0 +1,17 @@
+package interview;
+
+import org.junit.jupiter.api.Test;
+
+class TestD {
+
+	@Test
+	void test1() {
+		
+		String s1 = "abc";
+		String s2 = "abc";
+
+		System.out.println("s1 == s2 is: " + s1 == s2);
+		
+	}
+	
+}
